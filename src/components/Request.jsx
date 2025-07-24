716fd987a204453614ff608bf6826295366af8e1
Sk Mursalin <skmursalin02@gmail.com>
2025-07-24 12:04:06 +0530

    add review and reject feature

--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -2,34 +2,42 @@ import axios from 'axios'
 import React, { useEffect } from 'react'
 import { BASE_URL } from '../utils/constant'
 import { useDispatch, useSelector } from 'react-redux'
-import { addConnection } from '../store/slices/requestSlice'
+import { addConnection, removeConnection } from '../store/slices/requestSlice'
 
 const Request = () => {
     const connectionRequest = useSelector((state) => state.request)
     const dispatch = useDispatch()
     const fetchConnectionRequest = async () => {
         const res = await axios.get(BASE_URL + "/user/requests/recieved", { withCredentials: true });
-        dispatch(addConnection(res.data))
+        const data = res.data
+        dispatch(addConnection(data))
+        console.log(res.data);
     }
 
     useEffect(() => {
         fetchConnectionRequest()
     }, []);
+
+    const reviewRequestHandler = async (status,_id)=>{
+         await axios.post(`${BASE_URL}/connection/review/${status}/${_id}`,{},{withCredentials:true});
+         dispatch(removeConnection(_id));
+    }
+ 
+
     if (!connectionRequest) return
     if(connectionRequest.data.length === 0) {return <h1 className='text-center font-bold mt-3'> you don't have any request  </h1>}
     return (
         <div className='w-1/2  mx-auto'>
-            {connectionRequest.data.map(({ fromUserId }) => {
+            {connectionRequest.data.map((el) => {
                 return <div className="card w-full bg-base-300 card-xs shadow-sm mt-4">
                     <div className=" flex justify-between align-middle py-4 px-3">
                         <div className='w-11'>
-                            <img  className=" w-full h-11 rounded-full" src={fromUserId.photoUrl} alt="" />
+                            <img  className=" w-full h-11 rounded-full" src={el.fromUserId.photoUrl} alt="" />
                         </div>
-                        <h2 className="card-title">{`${fromUserId.firstName} ${fromUserId.lastName}`}</h2>
+                        <h2 className="card-title">{`${el.fromUserId.firstName} ${el.fromUserId.lastName}`}</h2>
                         <div className="justify-end card-actions">
-                            <button className="btn btn-secondary">accept</button>
-                            <button className="btn btn-accent">rereject</button>
-
+                            <button className="btn btn-secondary" onClick={()=>{reviewRequestHandler("accepted",el._id)}}>accept</button>
+                            <button className="btn btn-accent" onClick={()=>{reviewRequestHandler("rejected",el._id)}}>reject</button>
                         </div>
                     </div>
                 </div>
--- src/store/slices/requestSlice.js
+++ src/store/slices/requestSlice.js
@@ -1,16 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const requestSlice = createSlice({
-    name:"request",
-    initialState:null,
-    reducers:{
-        addConnection(state,action){
-            state = action.payload
-            return state
+    name: "request",
+    initialState: null,
+    reducers: {
+        addConnection(state, action) {
+            return action.payload
+
+        },
+        removeConnection(state, action) {
+            // const index = state.data.findIndex((el) => el._id === action.payload);
+            // state.data.splice(index, 1)
+
+            const newarray = state.data.filter((el)=> el._id !== action.payload)
+            state.data =  newarray
         }
     }
 });
 
 export default requestSlice.reducer;
 
-export const {addConnection} = requestSlice.actions
+export const { addConnection, removeConnection } = requestSlice.actions
