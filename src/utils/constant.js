1200348724bf96495d3c19283713314cd8920850
Sk Mursalin <skmursalin02@gmail.com>
2025-07-30 09:04:11 +0530

    add base_url for dev

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,8 @@
 
-export const BASE_URL = "/api";
+// export const BASE_URL = "/api";
+
+const isLocalhost = window.location.hostname === "localhost";
+
+export const BASE_URL = isLocalhost
+  ? "http://localhost:3000"
+  : "/api";
