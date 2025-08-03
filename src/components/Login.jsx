de6f75b8ecb32b9f6a62b3e0166b8b6889fd9f19
Sk Mursalin <skmursalin02@gmail.com>
2025-08-03 15:39:34 +0530

    add all validation fix bug of  from

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,28 +8,50 @@ import { validationConfig } from "../utils/validation";
 
 
 const Login = () => {
-    const [signupFormData, setSignupFormData] = useState({ email: "", password: "", firstName: "", lastName: "" });
+    const [signupFormData, setSignupFormData] = useState(() => {
+        const storedData = localStorage.getItem("signupFormData");
+        return storedData ? JSON.parse(storedData) : { firstName: "", lastName: "", email: "", password: "" };
+    });
     const [loginFormData, setLoginFromData] = useState(() => {
-    const storedData = localStorage.getItem("formData");
-    return storedData ? JSON.parse(storedData) : { email: "", password: "" };
-  });
-    const [isLogInForm, setIsLogInForm] = useState(true)
+        const storedData = localStorage.getItem("loginFormData");
+        return storedData ? JSON.parse(storedData) : { email: "", password: "" };
+    });
+    const [isLogInForm, setIsLogInForm] = useState(() => {
+        const storedData = localStorage.getItem("isLoginForm");
+        return storedData ? JSON.parse(storedData) : { condition: true }
+    });
+
     const [err, setErr] = useState()
-    const [validationData, setValidationData] = useState({});
+    const [signupValidationData, setSignupValidationData] = useState({});
     const [logValidationData, setlogValidationData] = useState({});
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
 
     useEffect(() => {
-        localStorage.setItem("formData", JSON.stringify(loginFormData));
+        const timeOut = setTimeout(() => {
+            localStorage.setItem("loginFormData", JSON.stringify(loginFormData));
+        }, 500)
+
+        return () => {clearTimeout(timeOut)}
     }, [loginFormData]);
 
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            localStorage.setItem("signupFormData", JSON.stringify(signupFormData));
+        }, 500);
+
+        return () => clearTimeout(timeout);
+    }, [signupFormData]);
+
+    useEffect(() => {
+        localStorage.setItem("isLoginForm", JSON.stringify(isLogInForm));
+    }, [isLogInForm]);
 
     const validation = () => {
         const copyValidationData = {};
 
-        Object.entries(isLogInForm ? loginFormData : signupFormData).forEach(([key, value]) => {
+        Object.entries(isLogInForm.condition ? loginFormData : signupFormData).forEach(([key, value]) => {
             validationConfig[key].some((rule) => {
                 if (!value && rule.require) {
                     copyValidationData[key] = rule.message
@@ -46,13 +68,13 @@ const Login = () => {
             })
         })
 
-        isLogInForm ? setlogValidationData(copyValidationData) : setValidationData(copyValidationData)
+        isLogInForm.condition ? setlogValidationData(copyValidationData) : setSignupValidationData(copyValidationData)
         return copyValidationData;
     }
 
     const formHandle = (e) => {
         const { name, value } = e.target
-        isLogInForm ? setLoginFromData((prvState) => ({ ...prvState, [name]: value })) : setSignupFormData((prvState) => ({ ...prvState, [name]: value }))
+        isLogInForm.condition ? setLoginFromData((prvState) => ({ ...prvState, [name]: value })) : setSignupFormData((prvState) => ({ ...prvState, [name]: value }))
     }
 
     const loginHandle = async () => {
@@ -102,44 +124,45 @@ const Login = () => {
     return (
         <div className="card card-border bg-base-300 max-w-96 screen400:mx-auto screen350:mx-2  mt-7 ">
             <div className="card-body">
-                <h2 className="card-title justify-center">{isLogInForm ? "Log IN" : "signUp"}</h2>
-                {!isLogInForm && <>
+                <h2 className="card-title justify-center">{isLogInForm.condition ? "Log IN" : "signUp"}</h2>
+                {!isLogInForm.condition && <>
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend font-normal">FirstName:</legend>
-                        <input type="text" name="firstName" className="input" placeholder="Type here" value={setSignupFormData.firstName}
+                        <input type="text" name="firstName" className="input" placeholder="Type here" value={signupFormData.firstName}
                             onChange={(e) => { formHandle(e) }}
                         />
-                        <p className="text-red-600">{validationData.firstName}</p>
+                        <p className="text-red-600">{signupValidationData.firstName}</p>
 
                     </fieldset>
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend font-normal">LastName:</legend>
-                        <input type="text" name="lastName" className="input" placeholder="Type here" value={setSignupFormData.lastName}
+                        <input type="text" name="lastName" className="input" placeholder="Type here" value={signupFormData.lastName}
                             onChange={(e) => { formHandle(e) }}
                         />
-                        <p className="text-red-600">{validationData.lastName}</p>
+                        <p className="text-red-600">{signupValidationData.lastName}</p>
 
                     </fieldset>
                 </>}
                 <fieldset className="fieldset">
                     <legend className="fieldset-legend font-normal">Email:</legend>
-                    <input type="text" name="email" className="input" placeholder="Type here" value={isLogInForm ? loginFormData.email : signupFormData.email}
+                    <input type="text" name="email" className="input" placeholder="Type here" value={isLogInForm.condition ? loginFormData.email : signupFormData.email}
                         onChange={(e) => { formHandle(e) }}
                     />
-                    <p className="text-red-600">{isLogInForm ? logValidationData.email : validationData.email}</p>
+                    <p className="text-red-600">{isLogInForm.condition ? logValidationData.email : signupValidationData.email}</p>
                 </fieldset>
                 <fieldset className="fieldset">
                     <legend className="fieldset-legend font-normal">Password:</legend>
-                    <input type="password" name="password" className="input" placeholder="Type here" value={isLogInForm ? loginFormData.password : signupFormData.password}
+                    <input type="password" name="password" className="input" placeholder="Type here" value={isLogInForm.condition ? loginFormData.password : signupFormData.password}
                         onChange={(e) => { formHandle(e) }}
                     />
-                    <p className="text-red-600">{isLogInForm ? logValidationData.password : validationData.password}</p>
+                    <p className="text-red-600">{isLogInForm.condition ? logValidationData.password : signupValidationData.password}</p>
                 </fieldset>
                 <p className="text-red-600">{err}</p>
                 <div className="card-actions justify-center">
-                    <button className="btn btn-primary" onClick={isLogInForm ? loginHandle : signUPHandle}>{isLogInForm ? "Log IN" : "signUp"}</button>
+                    <button className="btn btn-primary" onClick={isLogInForm.condition ? loginHandle : signUPHandle}>{isLogInForm.condition ? "Log IN" : "signUp"}</button>
                 </div>
-                <p className="cursor-pointer" onClick={() => { setIsLogInForm(!isLogInForm) }}>{isLogInForm ? "new user? sign up" : "have an account? login"}</p>
+                <p className="cursor-pointer"
+                    onClick={() => { setIsLogInForm((prvState) => { return !prvState.condition ? { condition: true } : { condition: false } }) }}>{isLogInForm.condition ? "new user? sign up" : "have an account? login"}</p>
             </div>
         </div>
     )
