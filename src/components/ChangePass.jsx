a5a06fd9bf4a84c54302c9725bf90924388e37f6
Sk Mursalin <skmursalin02@gmail.com>
2025-08-05 14:17:34 +0530

    fix ui of popup

--- src/components/ChangePass.jsx
+++ src/components/ChangePass.jsx
@@ -2,8 +2,8 @@ import { createPortal } from "react-dom"
 
 const ChangePass = ({ setPopUp }) => {
     return createPortal(
-        <div className="fixed inset-0 mx-auto top-40  ">
-            <div className="card card-border bg-base-100 max-w-96 screen400:mx-auto screen350:mx-2 ">
+        <div className="fixed inset-0 mx-auto  bg-black/40  ">
+            <div className="card card-border mt-44 bg-base-100 max-w-96 screen400:mx-auto screen350:mx-2 ">
                 <span className="ml-auto text-lg cursor-pointer pr-3 hover:text-red-700" onClick={() => { setPopUp(false) }}>⨯</span>
                 <div className="card-body w-full mx-auto ">
                     <h2 className="card-title justify-center">password change</h2>
