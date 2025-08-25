ce9c335de3a6dc9d5e2e29caafbdbd089458a81c
Sk Mursalin <skmursalin02@gmail.com>
2025-08-25 19:52:13 +0530

    fix profile friends ui

--- src/components/UserFriends.jsx
+++ src/components/UserFriends.jsx
@@ -4,7 +4,7 @@ export default function UserFriends({allFriends}) {
     <div className="max-w-2xl mx-auto p-4">
       <h2 className="text-xl font-bold mb-4">Friends</h2>
 
-      <div className="grid grid-cols-2 gap-4">
+      <div className="flex flex-col  gap-4">
         {allFriends.map((friend) => (
           <div
             key={friend._id}
